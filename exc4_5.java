0b633f2335f4ec6cb3b3eb7170b8913166eb7c78
MRxPosiTiVe <100093644+MRxPosiTiVe@users.noreply.github.com>
2022-12-16 17:28:38 +0500

    Add files via upload

--- exc4_5.java
+++ exc4_5.java
@@ -14,7 +14,7 @@ public class exc4_5 {
             y = exp(2 * pow(x, x)) + 5 * log10(2 * x + 2);
             System.out.println("y = " + y);
         } else if (x > 2){
-            y = (cos(x) - sin(x)) / pow(x, 2) + 5 * x + 6;
+            y = (cos(x) - sin(x)) / (pow(x, 2) + 5 * x + 6);
             System.out.println("y = " + y);
         } else if (x == 0 || x == 2) {
             y = 10000;
